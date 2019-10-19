835a6ee0c72663e7c987cef7c16e47ddd7046530
dynomite <gigit@github.com>
2019-10-19 14:00:28 +0600

    hw2 added

---
+++ hw2/2.py
@@ -0,0 +1,17 @@
+a = input()
+
+b = input()
+
+if "@" in a:
+
+    print('Некорректный логин')
+
+else:
+
+    if '@' in b:
+
+        print('OK')
+
+    else:
+
+        print('Некорректный адрес')
