16acae09ad749799cc1f4a5e989dc230d3973b87
chiro <nayapbergen@gmail.com>
2020-09-12 18:16:32 +0600

    CHANGE: fixed

--- 2nd_year/2/class/3.py
+++ 2nd_year/2/class/3.py
@@ -1,47 +1,31 @@
 PITCHES = ["до", "ре", "ми", "фа", "соль", "ля", "си"]
+LONG_PITCHES = ["до-о", "ре-э", "ми-и", "фа-а", "со-оль", "ля-а", "си-и"]
 
 
 class Note:
-    def __init__(self, x, is_long=False):
-        self.m = ["до", "ре", "ми", "фа", "соль", "ля", "си"]
-        self.n = ["до-о", "ре-э", "ми-и", "фа-а", "со-оль", "ля-а", "си-и"]
-        if x in self.m:
-            if is_long:
-                self.nt = self.n[self.m.index(x)]
-            else:
-                self.nt = x
-                
+    def __init__(self, pitch, is_long=False):
+        self.pitch = PITCHES.index(pitch)
+        self.is_long = is_long
+
     def __str__(self):
-        return (self.nt)
+        return LONG_PITCHES[self.pitch] if self.is_long else PITCHES[self.pitch]
+
+
+class DefaultNote(Note):
+    def __init__(self, pitch=PITCHES[0], is_long=False):
+        super().__init__(pitch, is_long)
 
 
 class LoudNote(Note):
     def __str__(self):
-        return (self.nt.upper())
-
-
-class DefaultNote(Note):
-    def __init__(self, x="до", is_long=False):
-        self.m = ["до", "ре", "ми", "фа", "соль", "ля", "си"]
-        self.n = ["до-о", "ре-э", "ми-и", "фа-а", "со-оль", "ля-а", "си-и"]
-        if x in self.m:
-            if is_long:
-                self.nt = self.n[self.m.index(x)]
-            else:
-                self.nt = x
+        return super().__str__().upper()
 
 
 class NoteWithOctave(Note):
-    def __init__(self, x, y, is_long=False):
-        self.m = ["до", "ре", "ми", "фа", "соль", "ля", "си"]
-        self.n = ["до-о", "ре-э", "ми-и", "фа-а", "со-оль", "ля-а", "си-и"]
-        if x in self.m:
-            if is_long:
-                self.nt = self.n[self.m.index(x)]
-            else:
-                self.nt = x
-            self.ny = y
+    def __init__(self, pitch, octave, is_long=False):
+        self.octave = octave
+        super().__init__(pitch, is_long)
 
     def __str__(self):
-        w = self.nt + " " + "(" + self.ny + ")"
-        return str(w)
+        return "{} ({})".format(super().__str__(), self.octave)
+
