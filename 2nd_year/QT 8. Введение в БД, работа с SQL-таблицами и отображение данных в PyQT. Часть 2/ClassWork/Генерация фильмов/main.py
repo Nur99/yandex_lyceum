036c7801891192e8b136586303d4d130ddcc537c
Alik <=ali.mars.99@gmail.com>
2020-11-07 17:32:11 +0600

    change: added генерация фильмов

--- 2nd_year/QT 8. Введение в БД, работа с SQL-таблицами и отображение данных в PyQT. Часть 2/ClassWork/Генерация фильмов/main.py
+++ 2nd_year/QT 8. Введение в БД, работа с SQL-таблицами и отображение данных в PyQT. Часть 2/ClassWork/Генерация фильмов/main.py
@@ -41,47 +41,41 @@ class MyWidget(QMainWindow, Ui_Form):
         super().__init__()
         self.setupUi(self)
         self.con = sqlite3.connect("films.db")
-        self.pushButton.clicked.connect(self.update_result)
-        self.tableWidget.itemChanged.connect(self.item_changed)
+        self.pushButton.clicked.connect(self.get_result)
         self.pushButton_2.clicked.connect(self.save_results)
         self.modified = {}
+        self.result = None
         self.titles = None
 
-    def update_result(self):
+    def get_result(self):
         cur = self.con.cursor()
         # Получили результат запроса, который ввели в текстовое поле
-        result = cur.execute(f"SELECT * FROM films WHERE id={self.textEdit.toPlainText()}").fetchall()
+        item_id = self.textEdit.toPlainText()
+        self.result = cur.execute(f"SELECT * FROM Films WHERE id={item_id}").fetchall()
         # Заполнили размеры таблицы
-        self.tableWidget.setRowCount(len(result))
+        self.tableWidget.setRowCount(len(self.result))
         # Если запись не нашлась, то не будем ничего делать
-        if not result:
+        if not self.result:
             self.statusBar().showMessage('Ничего не нашлось')
             return
         else:
             self.statusBar().showMessage(f"Нашлась запись с id = {item_id}")
-        self.tableWidget.setColumnCount(len(result[0]))
+        self.tableWidget.setColumnCount(len(self.result[0]))
         self.titles = [description[0] for description in cur.description]
         # Заполнили таблицу полученными элементами
-        for i, elem in enumerate(result):
+        for i, elem in enumerate(self.result):
             for j, val in enumerate(elem):
                 self.tableWidget.setItem(i, j, QTableWidgetItem(str(val)))
-        self.modified = {}
-
-    def item_changed(self, item):
-        # Если значение в ячейке было изменено,
-        # то в словарь записывается пара: название поля, новое значение
-        self.modified[self.titles[item.column()]] = item.text()
 
     def save_results(self):
-        if self.modified:
-            cur = self.con.cursor()
-            que = "UPDATE films SET\n"
-            for key in self.modified.keys():
-                que += "{}='{}'\n".format(key, self.modified.get(key))
-            que += "WHERE id = ?"
-            cur.execute(que, (self.spinBox.text(),))
-            self.con.commit()
-            self.modified.clear()
+        cur = self.con.cursor()
+        entry = self.result[0]
+        id = entry[0]
+        title = entry[1][::-1]
+        year = str(int(entry[2]) + 1000)
+        query = f"UPDATE Films SET title = '{title}', year = {year}, duration = duration * 2 WHERE id = {id}"
+        cur.execute(query).fetchall()
+        self.con.commit()
 
 
 if __name__ == '__main__':
